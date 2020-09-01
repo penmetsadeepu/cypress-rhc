afcbc44fe3a63283b330dba9b9fab9143443d4db
Vijay Manthena <vijay@vijay.com>
2020-09-01 20:54:08 +1000

    Final changes

--- RHC/rhc.spec.js
+++ RHC/rhc.spec.js
@@ -8,7 +8,7 @@ describe('RHC Login ', () => {
       cy.get('.submit').click();
       cy.wait(5000);
     });
-/*it('Edit to new Episode to Patients', () => {
+it('Edit to new Episode to Patients', () => {
     cy.get('span.mat-button-wrapper').contains('Patient').click();
     cy.wait(1000);
     cy.get('a[href*="#/patients/mypatients"]').click({force: true});
@@ -16,8 +16,8 @@ describe('RHC Login ', () => {
     cy.get('[type=checkbox]:eq(0)', { timeout: 10000}).check({force: true});
     cy.wait(10000);
     cy.get('[mattooltip="Edit Patient Record"]').click();
-    //cy.get('label.ng-star-inserted').as('include.text','Patient Type*')
-    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="RHC"]').click({force: true})
+    cy.get('label.ng-star-inserted').as('include.text','Patient Type*')
+    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="RHC"]').click({force: true})
     //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="RHH"]').click({force: true})
     cy.get(':nth-child(4) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix', { timeout: 2000}).click({multiple: true});
     cy.get('.mat-option-text', { timeout: 2000}).contains( 'Hyderabad', { timeout: 2000}).click({force: true});
@@ -26,25 +26,26 @@ describe('RHC Login ', () => {
     //cy.get('.mat-option-text', { timeout: 2000}).contains( 'physician1', { timeout: 2000}).click({force: true});
     cy.get('.mat-option-text', { timeout: 2000}).contains( 'physician2', { timeout: 2000}).click({force: true});
     cy.get('label.ng-star-inserted').as('include.text','Representative*')
-    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="No"]').click({force: true});
-    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Yes"]').click({force: true});
-    //cy.get('[formcontrolname="lasName"]',{timeout: 2000} ).type('newlastname');
-    //cy.get('[formcontrolname="firName"]').type('newfirstname');
-    //cy.get(':nth-child(6) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix', { timeout: 2000}).click({multiple: true});
-    //cy.get('.mat-option-text', { timeout: 2000}).contains('Spouse',{timeout: 2000}).click({force: true}); 
-    //cy.get('label.ng-star-inserted').as('include.text','Representative Type*')
-    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Patient Selected"]').click({force: true})
-    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Legally Authorized"]').click({force: true}) 
+    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="No"]').click({force: true});
+    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Yes"]').click({force: true});
+    cy.get('[formcontrolname="lasName"]',{timeout: 2000} ).type('newlastname');
+    cy.get('[formcontrolname="firName"]').type('newfirstname');
+    cy.get(':nth-child(8) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix',{ timeout: 2000}).click({force: true});
+    cy.get('.mat-option-text', { timeout: 2000}).contains('Spouse',{timeout: 2000}).click({force: true}); 
+    cy.get('label.ng-star-inserted').as('include.text','Representative Type*')
+    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Patient Selected"]').click({force: true})
+    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Legally Authorized"]').click({force: true}) 
     cy.get('label.ng-star-inserted').as('include.text','Guarantor Type*')
-    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Self"]').click({force: true})
-    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Spouse"]').click({force: true})
-    //cy.get('[formcontrolname="LastName"]',{timeout: 2000} ).type('newlastname');
-    //cy.get('[formcontrolname="FirstName"]').type('newfirstname');
-    //cy.get('[formcontrolname="address1"]').type('newaddress');
-    //cy.get('[formcontrolname="zipCode"]').type('3020');
-    //cy.get('[formcontrolname="city"]').type('newcity');
-    //cy.get('[formcontrolname="state"]').type('newstate');
-    //cy.get('[formcontrolname="phone"]').type('9441285727');
+    //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Self"]').click({force: true})
+    cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Spouse"]').click({force: true})
+    cy.get('[formcontrolname="LastName"]',{timeout: 2000} ).type('newlastname');
+    cy.get('[formcontrolname="FirstName"]').type('newfirstname');
+    cy.get('[formcontrolname="address1"]').type('newaddress');
+    cy.get('[formcontrolname="zipCode"]').type('23030');
+    cy.get('[formcontrolname="city"]').type('newcity');
+    cy.get(':nth-child(13) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix', { timeout: 2000}).click({multiple: true});
+    cy.get('.mat-option-text', { timeout: 2000}).contains( 'CA', { timeout: 2000}).click({force: true});
+    cy.get('[formcontrolname="phone"]').type('9441285727');
     cy.get('label.ng-star-inserted').as('include.text','Payment')
     cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Commercial Insurance"]').click({force: true})
     //cy.get('mat-radio-button.mat-radio-button.mat-accent', { timeout: 2000}).find('[value="Private Pay"]').click({force: true})
@@ -52,7 +53,7 @@ describe('RHC Login ', () => {
     //cy.get('[formcontrolname="medicareNum"]').type('5342879');
     cy.get('span.mat-button-wrapper').contains('Submit').click({force: true});
     cy.wait(2000);  
-})*/
+})
 
 
    /* it('uploading file',() => {
@@ -76,7 +77,7 @@ describe('RHC Login ', () => {
 })*/
 
 
-it('Export csv',() => {
+/*it('Export csv',() => {
     cy.get('span.mat-button-wrapper').contains('Patient').click();
     cy.wait(1000);
     cy.get('a[href*="#/patients/mypatients"]',{ timeout: 10000}).click({force: true});
@@ -90,7 +91,7 @@ it('Export csv',() => {
     //cy.get('span.mat-button-wrapper').contains('Cancel').click();
     cy.get('span.mat-button-wrapper').contains('Export').click({force: true});
    
-})
+})*/
 })
 
 
